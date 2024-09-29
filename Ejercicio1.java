ca934d2675fd1654744323dc808af35fa5c5ec20
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2024-09-29 22:37:41 +0200

    probando probando

---
+++ Ejercicio1.java
@@ -0,0 +1,34 @@
+public class Ejercicio1 {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+        float a, b, c, d, e, f, g, h;
+        System.out.println("el resultado de a es: ");
+        a=((3+2)%2-15)/2*5;
+        System.out.println(a);
+        System.out.println("  ");
+        System.out.println("el resultado de b es: ");
+        b=(6 + (6) / 7) + 35 / 2 -8 * 5 / 4 * 2;
+        System.out.println(b);
+        System.out.println("  ");
+        System.out.println("el resultado de c es: ");
+        c =3 + 6 * 14 % 3;
+        System.out.println(c);
+        System.out.println("  ");
+        System.out.println("el resultado de d es: ");
+        d=8 + 7 * 3 + 4 * 6 /2 % 4;
+        System.out.println(d);
+        System.out.println("el resultado de e es: ");
+        e=27 % 4 +15 / 4;
+        System.out.println(e);
+        System.out.println("el resultado de f es: ");
+        f=37 / 16 - 2;
+        System.out.println(f);
+        System.out.println("el resultado de g es: ");
+        g=9 * 2 / 3 * 25 * 3 ;
+        System.out.println(g);
+        System.out.println("el resultado de h es: ");
+        h=(7 * 3 - 4 * 4) * 2 / 4 * 2;
+        System.out.println(h);
+    }
+}
