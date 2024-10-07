61a0439f30c8f25e72c8a1bbb83cac7ea848dfd9
Daniel Figueroa Vidal <dfigueroavidal@danielcastelao.org>
2024-10-07 21:39:31 +0200

    terminado el ejercicio 5

---
+++ src/Boletin3_5.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Boletin3_5 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int n1, n2, n3;
+        System.out.println("Dime el primer numero");
+        n1 = teclado.nextInt();
+        System.out.println("Dime el segundo numero");
+        n2 = teclado.nextInt();
+        System.out.println("Dime el tercer numero");
+        n3 = teclado.nextInt();
+        if (n1 > n2) {
+            if (n1 > n3) {
+                System.out.println("el primer numero es el mayor");
+            }else if(n1<n3){
+                System.out.println("El tercer numero es el mayor");
+            }
+        } else if (n2 > n1) {
+                if (n2 > n3) {
+                    System.out.println("El segundo numero es el mayor");
+                }
+                else if(n2<n3){
+                    System.out.println("El tercer numero es el mayor");
+                }
+            }
+        }
+}
